2a4652e7057a85c52a51bc75c1b249df2202a1bd
Ben Spaulding <ben@benspaulding.com>
2009-01-19 16:23:55 -0600

    Getting close. Needs cleanup and, a little detail called regex. :)

--- port-templates.py
+++ port-templates.py
@@ -5,23 +5,27 @@
 django-template-porting-util: Update Django template syntax from .9x to 1.0.
 
 This program automates much of the process of moving your Django templates from
-Django .9x to Django 1.0. It is safe to re-run this program on templates, if
+Django .9x to Django 1.0. It is safe to re-run this program on templates if,
 for example, you are porting templates while the old templates are still in use
-subject to change. Just put any new templates in the template directory and
-re-run. Remember that you can also point to a specific directory, like a
-specific directory within your template directory.
+and subject to change. Just put any new templates in the template directory and
+re-run. Remember that you can also point to a specific directory, or even a
+specific template.
+
+WARNING: This program irreversibly alters your templates! It is essential that
+you use some for of version control or at least make backups of your files
+before every use.
 
 """
 # Note that __future__ imports must go at the top.
 from __future__ import with_statement
 
 __author__ = "Ben Spaulding"
-__contributors__ = ["Christian Metts", "Travis Cline"]
+__contributors__ = []
 __copyright__ = "(c) 2008 Ben Spaulding. GNU GPL 3."
 __description__ = "Update Django template syntax from .9x to 1.0."
 __url__ = "http://github.com/benspaulding/django-template-porting-util"
 
-import sys, os
+import sys, os, pprint, yaml
 from optparse import OptionParser
 
 try:
@@ -39,12 +43,13 @@ class TemplateMonkey:
 
     def __init__(self, options, args):
         self.options = options
+        # Q: Do I need args here? Don’t know when or why I would use them.
         self.args = args
         try:
-            # TODO: Am I doing this right? The django.conf.settings and
-            #       django.db.models relationship leaves me confused as
-            #       to how this needs implemented and when django.db.models
-            #       should be imported.
+            # Q: Am I doing this right? The django.conf.settings and
+            #    django.db.models relationship leaves me confused as
+            #    to how this needs implemented and when django.db.models
+            #    should be imported.
             self.settings = __import__(self.options.settings)
             os.environ["DJANGO_SETTINGS_MODULE"] = self.options.settings
         except ImportError:
@@ -53,23 +58,22 @@ class TemplateMonkey:
             #       could be disterous.
             print u"Cannot load the specified settings module; using DJANGO_SETTINGS_MODULE environment variable instead."
             self.settings = settings
+        # Q: Should the config keys and template_paths be added as
+        #    attrs to the class here? Dunno.
+        # Q: Does this belong here? I’m thinking of verbosity.
+        # self.pprinter = pprint.PrettyPrinter(indent=2)
 
     def port_templates(self):
-        """
+        """Run requested methods on the specified templates."""
 
-
-        """
         if not self.options.add_extension and \
            not self.options.update_file_fields and \
            not self.options.update_relations:
             print u"This monkey won’t do anything unless you tell it to — see available options by running “port-templates.py --help”"
             sys.exit()
 
-        import pdb; pdb.set_trace()
-
         template_paths = self.create_template_paths()
-
-        config_dict = self.generate_dicts()
+        config = self.load_config()
 
         for template_path in template_paths:
             # Using “with” statements with file objcts is good practice, per
@@ -79,7 +83,7 @@ class TemplateMonkey:
             with open(template_path, 'r+') as template:
                 for line in template:
                     # Now call each method on every line of the template, as needed.
-                    # Do I need to flush anywhere for this to work?
+                    # Q: Do I need to flush anywhere for this to work?
                     if self.options.add_extension:
                         self.add_extension(line)
                     if self.options.update_file_fields:
@@ -88,7 +92,7 @@ class TemplateMonkey:
                         self.update_relations(line)
                 # TODO: Here (I believe) is where I need to write each line
                 #       back to the template file. How?
-                template.write(template)
+                # template.write(template)
                 template.close()
 
 
@@ -98,7 +102,7 @@ class TemplateMonkey:
 
         This is done using one of the following, in this order:
 
-        1. The arguments passed to any --template-path options
+        1. The arguments passed to any --template-path options,
         2. The directories in in TEMPLATE DIRS of the settings
            module (supplied by the --settings option or the
            DJANGO_SETTINGS_MODULE environment variable).
@@ -119,35 +123,50 @@ class TemplateMonkey:
             elif os.path.isfile(path):
                 template_paths.append(path)
 
-        # import pprint
-        # pp = pprint.PrettyPrinter(indent=4)
-        # print pp.pprint(template_paths)
-        # print u"I created the template paths list!"
         if not template_paths:
             print u"You either failed to provide any template paths (via settings.TEMPLATE_DIRS or the --template-path option), or those you specified do not exist."
             sys.exit()
         return template_paths
             
 
-    def generate_dicts(self):
+    def load_config(self):
         """
+        Create configuration dictionary from various sources.
 
+        Loads the specified YAML configuration file and adds all
+        methods found on any model in settings.INSTALLED_APPS to
+        the config.relations.ignored_methods list. 
 
         """
-        pass
-        # 
-        # for model in models.get_models():
-        #     opts = model._meta
-        #     print opts
-        #     for field in opts.fields:
-        #         print "  f  %s" % field.name
-        #     for func_name, func in model.__dict__.items():
-        #         try:
-        #             if not func_name.startswith("get_"):
-        #                 raise StopIteration
-        #         except StopIteration:
-        #             continue
-        #         print "  m  %s" % func_name
+        # Q: Do I need exception handline for the file stuff?
+        config_file = open(self.options.config_path)
+        config = yaml.load(config_file)
+        config_file.close()
+
+        # Append all get_* methods from all models to the list
+        # of methods that will always be left untouched.
+        for model in models.get_models():
+            for func_name, func in model.__dict__.items():
+                if func_name.startswith("get_"):
+                    config['relations']['ignored_methods'].append(func_name)
+
+        # Remove duplicate methods, just to be tidy.
+        # Done via a dictionary because it’s faster; see
+        # http://pyfaq.infogami.com/how-do-you-remove-duplicates-from-a-list
+        d = {}
+        for method in config['relations']['ignored_methods']:
+            d[method]=method
+
+        # Now remove methods from the ignored_methods list that
+        # are to be updated despite being actual methods.
+        for method in config['relations']['force_update']:
+            del d[method]
+        # Remove this key as it’s no longer needed.
+        del config['relations']['force_update']
+
+        # Now put the cleaned-up dictionary values back into our list.
+        config['relations']['ignored_methods'] = d.values()
+        return config
 
 
     def add_extension(self, line):
@@ -166,11 +185,11 @@ class TemplateMonkey:
             {% include "<template>.html" %}
 
         Note that any references that already end with any of the extensions
-        listed in config.yml will be skipped.
+        listed in config.extensions will be skipped.
 
         """
         # Use regex fu on the line.
-        return u"This is foo."
+        return line
 
 
     def update_file_fields(self, line):
@@ -190,8 +209,9 @@ class TemplateMonkey:
 
         All matches will be replaced with the following exceptions:
 
-        1. matches listed in the config.relations.skip,
-        2. any actual model methods found via settings.INSTALLED_APPS.
+        1. matches listed in the config.relations.ignored_methods,
+        2. any actual model methods found via settings.INSTALLED_APPS,
+           excluding those listed in config.relations.force_update.
 
         """
         # Use regex fu on the line.
@@ -202,19 +222,22 @@ class TemplateMonkey:
         """
         Fix references to related objects.
 
-        =============  =========
+        =============  =============
         Old syntax…    Becomes…
-        -------------  ---------
+        -------------  -------------
         get_foo        foo
-        get_foo_list   foo.all
-        get_foo_count  foo.count
-        =============  =========
+        get_foo_list   foo_set.all
+        get_foo_count  foo_set.count
+        =============  =============
 
         All matches will be replaced with the following exceptions:
 
-        1. matches listed in the config.relations.skip,
+        1. matches listed in the config.relations.ignored_methods,
         2. any actual model methods found via settings.INSTALLED_APPS,
-           exlucding those listed in config.relations.update.
+           excluding those listed in config.relations.force_update.
+
+        Note that ``foo_set`` all and count replacements can be customized
+        to account for related_name attributes via config.relations.mapping.
 
         """
         # Use regex fu on the line.
@@ -233,24 +256,27 @@ if __name__ == "__main__":
         ["-q", "--quiet", dict(
                                 dest="verbose", action="store_false",
                                 help=u"output nothing to the console")],
+        ["-n", "--dry-run", dict(
+                                dest="dry_run", action="store_true",
+                                help=u"run everything as normal but don’t save any changes")],
         ["-x", "--add-extension", dict(
                                 dest="add_extension", action="store_true",
                                 help=u"add extension “.html” to template references in {% extends %} and {% include %} tags")],
         ["-f", "--update-file-fields", dict(
                                 dest="update_file_fields", action="store_true",
-                                help=u"update old file methods to new attributes, i.e. get_baz_url => baz.url, get_baz_size => baz.size, etc.")],
+                                help=u"update old file methods to new attributes, i.e. get_foo_url => foo.url, get_foo_size => foo.size, etc.")],
         ["-r", "--update-relations", dict(
                                 dest="update_relations", action="store_true",
-                                help=u"update old relation methods to new attributes, i.e. get_foo => foo, get_bar_list => bar_set.all, etc.")],
+                                help=u"update old relation methods to new attributes, i.e. get_bar => bar, get_baz_list => baz_set.all, etc.")],
         ["-s", "--settings", dict(
                                 dest="settings", action="store", default=os.environ["DJANGO_SETTINGS_MODULE"],
                                 help=u"use the given settings module (default to $DJANGO_SETTINGS_MODULE)", metavar="settings.module")],
         ["-t", "--template-path", dict(
                                 dest="template_paths", action="append", default=[],
-                                help=u"work on the given path (default to settings.TEMPLATE_DIRS)", metavar="/path/to/template(s)")],
+                                help=u"work on the given path (default to settings.TEMPLATE_DIRS)", metavar="/path/to/templates")],
         ["-y", "--yaml-config", dict(
-                                dest="yaml_config", action="store", default="config.yml", metavar="/path/to/file.yml",
-                                help=u"items listed in the given file will not be touched (default to config.yml)")],
+                                dest="config_path", action="store", default="config.yml", metavar="/path/to/file.yml",
+                                help=u"use the specified YAML file for special-case exceptions. (default to config.yml)")],
     ]
 
     for s, l, k in options_array:
--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name='django-template-porting-util',
     version='pre-1.0',
     description='Python module for porting Django templates from .9x versions to 1.0',
-    author='Mediaphormedia, L.L.C.',
+    author='Ben Spaulding',
     py_modules=['django-template-porting-util'])
