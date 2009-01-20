4490f6f1e0e3bbf7ee87b96608d19bfd729c42fc
Ben Spaulding <ben@benspaulding.com>
2009-01-20 11:16:26 -0600

    Made lots of changes and fixes with the help of Daniel Lindsley.

--- port-templates.py
+++ port-templates.py
@@ -12,7 +12,7 @@ re-run. Remember that you can also point to a specific directory, or even a
 specific template.
 
 WARNING: This program irreversibly alters your templates! It is essential that
-you use some for of version control or at least make backups of your files
+you use some form of version control or at least make backups of your files
 before every use.
 
 """
@@ -20,17 +20,19 @@ before every use.
 from __future__ import with_statement
 
 __author__ = "Ben Spaulding"
-__contributors__ = []
-__copyright__ = "(c) 2008 Ben Spaulding. GNU GPL 3."
+__contributors__ = ["Daniel Lindsley",]
+__copyright__ = "(c) 2008 Ben Spaulding. New BSD."
 __description__ = "Update Django template syntax from .9x to 1.0."
 __url__ = "http://github.com/benspaulding/django-template-porting-util"
+__version__ = "0.9"
 
-import sys, os, pprint, yaml
+import os
+import sys
+import yaml
+from pprint import PrettyPrinter
 from optparse import OptionParser
 
 try:
-    from django.conf import settings
-    from django.db import models
     from django.template import BLOCK_TAG_START, BLOCK_TAG_END
     from django.template import VARIABLE_TAG_START, VARIABLE_TAG_END
     from django.template import COMMENT_TAG_START, COMMENT_TAG_END
@@ -39,29 +41,27 @@ except ImportError:
     sys.exit()
 
 
-class TemplateMonkey:
+class TemplateMonkey(object):
 
     def __init__(self, options, args):
         self.options = options
-        # Q: Do I need args here? Don’t know when or why I would use them.
+        # Q: Do I need args here? Don’t know when or why I’d use them.
         self.args = args
+
         try:
-            # Q: Am I doing this right? The django.conf.settings and
-            #    django.db.models relationship leaves me confused as
-            #    to how this needs implemented and when django.db.models
-            #    should be imported.
             self.settings = __import__(self.options.settings)
             os.environ["DJANGO_SETTINGS_MODULE"] = self.options.settings
         except ImportError:
-            # TODO: Don’t just inform here — prompt and ask for confirmation
-            #       to continue. Accidently running this on a template directory
-            #       could be disterous.
-            print u"Cannot load the specified settings module; using DJANGO_SETTINGS_MODULE environment variable instead."
-            self.settings = settings
-        # Q: Should the config keys and template_paths be added as
-        #    attrs to the class here? Dunno.
-        # Q: Does this belong here? I’m thinking of verbosity.
-        # self.pprinter = pprint.PrettyPrinter(indent=2)
+            print u"Cannot load the specified settings module."
+            sys.exit()
+
+        self.printer = PrettyPrinter(indent=2)
+        config = self.load_config()
+        self.extensions = config["extensions"]
+        self.list_count_map = config["list_count_map"]
+        self.ignored_methods = config["ignored_methods"]
+        self.template_paths = self.create_template_paths(config["template_paths"])
+
 
     def port_templates(self):
         """Run requested methods on the specified templates."""
@@ -72,15 +72,12 @@ class TemplateMonkey:
             print u"This monkey won’t do anything unless you tell it to — see available options by running “port-templates.py --help”"
             sys.exit()
 
-        template_paths = self.create_template_paths()
-        config = self.load_config()
-
-        for template_path in template_paths:
+        for template_path in self.template_paths:
             # Using “with” statements with file objcts is good practice, per
             # http://docs.python.org/tutorial/inputoutput.html
-            # Note that “with” statements had to be enabled in Python 2.5. See
+            # Note that “with” statements have to be enabled in Python 2.5. See
             # http://docs.python.org/whatsnew/2.5.html#pep-343-the-with-statement
-            with open(template_path, 'r+') as template:
+            with open(template_path, "r+") as template:
                 for line in template:
                     # Now call each method on every line of the template, as needed.
                     # Q: Do I need to flush anywhere for this to work?
@@ -93,10 +90,60 @@ class TemplateMonkey:
                 # TODO: Here (I believe) is where I need to write each line
                 #       back to the template file. How?
                 # template.write(template)
-                template.close()
 
 
-    def create_template_paths(self):
+    def load_config(self):
+        """
+        Create configuration dictionary from various sources.
+
+        Loads the specified YAML configuration file and adds all
+        methods found on any model in settings.INSTALLED_APPS to
+        the ignored_methods list, and then removes any methods
+        in force_update from ignored_methods.
+
+        """
+        from django.db import models
+
+        # Q: Should I be using a try/except block or while?
+        # Q: If with is used, how do I handle exceptions in a with statement?
+        # Q: What if config_file is moved/altered while after being opened,
+        #    and before being closed? Anything?
+        # try:
+        #     config_file = open(self.options.config_path)
+        # except IOError:
+        #     print u"The specified configuration file could not be found."
+        #     sys.exit()
+        # config = yaml.load(config_file)
+        # config_file.close()
+        with open(self.options.config_path) as config_file:
+            config = yaml.load(config_file)
+
+        # Append all get_* methods from all models to the list
+        # of methods that will always be left untouched.
+        # Use a set because it’s fast and avoids duplicates.
+        # Q: If the YAML syntax is wrong we will get a confusing
+        #    error message here. Should we handle that exception?
+        s = set(config["ignored_methods"])
+        for model in models.get_models():
+            for func_name, func in model.__dict__.items():
+                if func_name.startswith("get_"):
+                    s.add(func_name)
+
+        # Now remove methods from the ignored_methods list that
+        # are to be updated despite being actual methods.
+        for method in config["force_update"]:
+            try: s.remove(method)
+            except KeyError: pass
+
+        # Remove this key as it’s no longer needed. This may be overkill.
+        del config["force_update"]
+
+        # Now drop the set back into our list.
+        config["ignored_methods"] = list(s)
+        return config
+
+
+    def create_template_paths(self, config_paths):
         """
         Generate a complete list of templates to be worked on.
 
@@ -110,7 +157,7 @@ class TemplateMonkey:
         """
         template_paths = []
 
-        for path in self.options.template_paths or self.settings.TEMPLATE_DIRS:
+        for path in self.options.template_paths or config_paths or self.settings.TEMPLATE_DIRS:
             # Generally directories will be given to us.
             if os.path.isdir(path):
                 for dirpath, dirnames, filenames in os.walk(path):
@@ -124,49 +171,9 @@ class TemplateMonkey:
                 template_paths.append(path)
 
         if not template_paths:
-            print u"You either failed to provide any template paths (via settings.TEMPLATE_DIRS or the --template-path option), or those you specified do not exist."
+            print u"You either failed to provide any template paths (via the --template-path option, config.template_paths, or settings.TEMPLATE_DIRS), or those you specified do not exist."
             sys.exit()
         return template_paths
-            
-
-    def load_config(self):
-        """
-        Create configuration dictionary from various sources.
-
-        Loads the specified YAML configuration file and adds all
-        methods found on any model in settings.INSTALLED_APPS to
-        the config.relations.ignored_methods list. 
-
-        """
-        # Q: Do I need exception handline for the file stuff?
-        config_file = open(self.options.config_path)
-        config = yaml.load(config_file)
-        config_file.close()
-
-        # Append all get_* methods from all models to the list
-        # of methods that will always be left untouched.
-        for model in models.get_models():
-            for func_name, func in model.__dict__.items():
-                if func_name.startswith("get_"):
-                    config['relations']['ignored_methods'].append(func_name)
-
-        # Remove duplicate methods, just to be tidy.
-        # Done via a dictionary because it’s faster; see
-        # http://pyfaq.infogami.com/how-do-you-remove-duplicates-from-a-list
-        d = {}
-        for method in config['relations']['ignored_methods']:
-            d[method]=method
-
-        # Now remove methods from the ignored_methods list that
-        # are to be updated despite being actual methods.
-        for method in config['relations']['force_update']:
-            del d[method]
-        # Remove this key as it’s no longer needed.
-        del config['relations']['force_update']
-
-        # Now put the cleaned-up dictionary values back into our list.
-        config['relations']['ignored_methods'] = d.values()
-        return config
 
 
     def add_extension(self, line):
@@ -209,9 +216,9 @@ class TemplateMonkey:
 
         All matches will be replaced with the following exceptions:
 
-        1. matches listed in the config.relations.ignored_methods,
+        1. matches listed in the ignored_methods,
         2. any actual model methods found via settings.INSTALLED_APPS,
-           excluding those listed in config.relations.force_update.
+           excluding those listed in force_update.
 
         """
         # Use regex fu on the line.
@@ -232,12 +239,12 @@ class TemplateMonkey:
 
         All matches will be replaced with the following exceptions:
 
-        1. matches listed in the config.relations.ignored_methods,
+        1. matches listed in the ignored_methods,
         2. any actual model methods found via settings.INSTALLED_APPS,
-           excluding those listed in config.relations.force_update.
+           excluding those listed in force_update.
 
         Note that ``foo_set`` all and count replacements can be customized
-        to account for related_name attributes via config.relations.mapping.
+        to account for related_name attributes via list_count_map.
 
         """
         # Use regex fu on the line.
@@ -274,7 +281,7 @@ if __name__ == "__main__":
         ["-t", "--template-path", dict(
                                 dest="template_paths", action="append", default=[],
                                 help=u"work on the given path (default to settings.TEMPLATE_DIRS)", metavar="/path/to/templates")],
-        ["-y", "--yaml-config", dict(
+        ["-c", "--config-yaml", dict(
                                 dest="config_path", action="store", default="config.yml", metavar="/path/to/file.yml",
                                 help=u"use the specified YAML file for special-case exceptions. (default to config.yml)")],
     ]
