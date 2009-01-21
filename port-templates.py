121c54d0760d2aa4a37f7d76bc759911206399ad
Ben Spaulding <ben@benspaulding.com>
2009-01-21 02:15:42 -0600

    Implemented my new quote convention.
    I decided to follow this convention_ after doing some
    research and concluded that there is no difference 
    between single and double quotes in Python (see the
    docs_ particularly).
    
    .. _convention: http://mail.python.org/pipermail/python-list/2005-July/332691.html
    .. _docs: http://docs.python.org/reference/lexical_analysis.html#strings

--- port-templates.py
+++ port-templates.py
@@ -38,7 +38,7 @@ try:
     from django.template import BLOCK_TAG_START, BLOCK_TAG_END
     from django.template import VARIABLE_TAG_START, VARIABLE_TAG_END
     from django.template import COMMENT_TAG_START, COMMENT_TAG_END
-    DJANGO_SETTINGS_MODULE = os.environ["DJANGO_SETTINGS_MODULE"]
+    DJANGO_SETTINGS_MODULE = os.environ['DJANGO_SETTINGS_MODULE']
 except (ImportError, KeyError):
     print u"Cannot find Django. Did you setup your environment correctly? (Don’t forget to set the DJANGO_SETTINGS_MODULE environment variable.)"
     sys.exit()
@@ -50,17 +50,17 @@ class TemplateMonkey(object):
 
         try:
             self.settings = __import__(options.settings)
-            os.environ["DJANGO_SETTINGS_MODULE"] = options.settings
+            os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
         except ImportError:
             print u"Cannot load settings module."
             sys.exit()
 
         self.printer = PrettyPrinter(indent=2)
         config = self.load_config()
-        self.extensions = config["extensions"]
-        self.related_names = config["related_names"]
-        self.ignored_methods = config["ignored_methods"]
-        self.template_paths = config["template_paths"]
+        self.extensions = config['extensions']
+        self.related_names = config['related_names']
+        self.ignored_methods = config['ignored_methods']
+        self.template_paths = config['template_paths']
 
         # Compile the regexen here for speed.
         self.extension_regex = re.compile('{%\s+(?P<tag>extends|include)\s+(\"|\')(?P<file_path>.*?)(\"|\')\s+%}')
@@ -84,10 +84,10 @@ class TemplateMonkey(object):
             # http://docs.python.org/tutorial/inputoutput.html
             # Note that “with” statements have to be enabled in Python 2.5. See
             # http://docs.python.org/whatsnew/2.5.html#pep-343-the-with-statement
-            with open(template_path, "r+") as template:
+            with open(template_path, 'r+') as template:
                 # Stow the original.
                 original_template = template
-                ported_template = ""
+                ported_template = ''
 
                 for line in template:
                     # Now call each method on every line of the template, as needed.
@@ -106,8 +106,8 @@ class TemplateMonkey(object):
                     ported_template_file.close()
                 else:
                     from difflib import unified_diff
-                    print "Diff for '%s'" % template_path
-                    print "".join(unified_diff(original_template, ported_template))
+                    print u"Diff for ‘%s’" % template_path
+                    print u"".join(unified_diff(original_template, ported_template))
                     print
 
 
@@ -142,26 +142,26 @@ class TemplateMonkey(object):
         # Use a set because it’s fast and avoids duplicates.
         # Q: If the YAML syntax is wrong we will get a confusing
         #    error message here. Should we handle that exception?
-        config["ignored_methods"] = set(config["ignored_methods"])
+        config['ignored_methods'] = set(config['ignored_methods'])
         for model in models.get_models():
             for func_name, func in model.__dict__.items():
-                if func_name.startswith("get_"):
-                    config["ignored_methods"].add(func_name)
+                if func_name.startswith('get_'):
+                    config['ignored_methods'].add(func_name)
 
         # Now remove methods from the ignored_methods list that
         # are to be updated despite being actual methods.
-        for method in config["force_update"]:
+        for method in config['force_update']:
             try:
-                config["ignored_methods"].remove(method)
+                config['ignored_methods'].remove(method)
             except KeyError:
                 pass
 
         # Remove this key as it’s no longer needed. (This may be overkill.)
-        del config["force_update"]
+        del config['force_update']
 
         # This is the place to do this, but the execution looks a little
         # wonky. Maybe there is a more graceful way to do this?
-        config["template_paths"] = self.create_template_paths(config["template_paths"])
+        config['template_paths'] = self.create_template_paths(config['template_paths'])
 
         return config
 
@@ -186,7 +186,7 @@ class TemplateMonkey(object):
                 for dirpath, dirnames, filenames in os.walk(path):
                     # TODO: exclude .* directories.
                     for filename in filenames:
-                        if not filename.startswith("."):
+                        if not filename.startswith('.'):
                             template_paths.append(os.path.join(dirpath, filename))
 
             # But occasionally someone might hand us a single template file.
@@ -349,43 +349,43 @@ class ReplacementTestCase(unittest.TestCase):
             self.assertEqual(self.monkey.update_relations(old_template), new_template)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     usage = """%prog [options]"""
     desc = __doc__
 
     parser = OptionParser(usage=usage, description=desc)
-    parser.add_option("-q", "--quiet",
-                                dest="verbosity", action="store_false",
+    parser.add_option('-q', '--quiet',
+                                dest='verbosity', action='store_false',
                                 help=u"output nothing to the console")
-    parser.add_option("-v", "--verbose",
-                                dest="verbosity", action="store_true",
+    parser.add_option('-v', '--verbose',
+                                dest='verbosity', action='store_true',
                                 help=u"output all information to the console")
-    parser.add_option("-n", "--dry-run",
-                                dest="dry_run", action="store_true",
+    parser.add_option('-n', '--dry-run',
+                                dest='dry_run', action='store_true',
                                 help=u"run everything as normal but don’t save any changes")
-    parser.add_option("-x", "--add-extension",
-                                dest="add_extension", action="store_true",
+    parser.add_option('-x', '--add-extension',
+                                dest='add_extension', action='store_true',
                                 help=u"add extension “.html” to template references in {% extends %} and {% include %} tags")
-    parser.add_option("-f", "--update-file-fields",
-                                dest="update_file_fields", action="store_true",
+    parser.add_option('-f', '--update-file-fields',
+                                dest='update_file_fields', action='store_true',
                                 help=u"update old file methods to new attributes, i.e. get_foo_url => foo.url, get_foo_size => foo.size, etc.")
-    parser.add_option("-r", "--update-relations",
-                                dest="update_relations", action="store_true",
+    parser.add_option('-r', '--update-relations',
+                                dest='update_relations', action='store_true',
                                 help=u"update old relation methods to new attributes, i.e. get_bar => bar, get_baz_list => baz_set.all, etc.")
-    parser.add_option("-s", "--settings",
-                                dest="settings", action="store", default=DJANGO_SETTINGS_MODULE,
+    parser.add_option('-s', '--settings',
+                                dest='settings', action='store', default=DJANGO_SETTINGS_MODULE,
                                 help=u"use the given settings module (default to $DJANGO_SETTINGS_MODULE)", metavar="settings.module")
-    parser.add_option("-t", "--template-path",
-                                dest="template_paths", action="append", default=[],
+    parser.add_option('-t', '--template-path',
+                                dest='template_paths', action='append', default=[],
                                 help=u"work on the given path (default to settings.TEMPLATE_DIRS)", metavar="/path/to/templates")
-    parser.add_option("-c", "--config-yaml",
-                                dest="config_path", action="store", default="config.yml", metavar="/path/to/file.yml",
+    parser.add_option('-c', '--config-yaml',
+                                dest='config_path', action='store', default='config.yml', metavar='/path/to/file.yml',
                                 help=u"use the specified YAML file for special-case exceptions. (default to config.yml)")
-    parser.add_option("-T", "--run-tests",
-                                dest="run_tests", action="store_true",
+    parser.add_option('-T', '--run-tests',
+                                dest='run_tests', action='store_true',
                                 help=u"run unit tests for this program")
 
-    (globals()["options"], args) = parser.parse_args()
+    (globals()['options'], args) = parser.parse_args()
 
     if options.run_tests:
         suite = unittest.TestLoader().loadTestsFromTestCase(ReplacementTestCase)
