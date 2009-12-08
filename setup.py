e3c7b12be1a6434eff535a1a359387827eb62d53
Ben Spaulding <ben@benspaulding.com>
2009-12-08 16:29:32 -0600

    Removed setup.py.
    I don't have the time to get this working. Ever. Just
    use the script in place.

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(name='django-template-porting-util',
-    version='.9',
-    description='Python module for porting Django templates from .9x versions to 1.0',
-    author='Ben Spaulding',
-    py_modules=['django-template-porting-util'])
